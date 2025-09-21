ea83e3f86d1db3f037e9baf2f30e8a89e500f068
Kieran Bennett <kierancliffbennett@gmail.com>
2025-09-21 12:02:18 +1000

    css adjustments for chef claude

--- src/components/ChefClaude/ClaudeRecipe.jsx
+++ src/components/ChefClaude/ClaudeRecipe.jsx
@@ -3,6 +3,7 @@ import ReactMarkdown from "react-markdown"
 export default function ClaudeRecipe(props) {
     return (
         <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef Recommends:</h2>
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
